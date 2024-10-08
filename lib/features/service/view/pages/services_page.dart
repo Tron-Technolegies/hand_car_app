99adcac22383d426ce49f43ba3dfd60e90727ed5
risanpallithazha <risanpallithazha@gmail.com>
2024-10-08 09:25:14 +0530

    Services Icons Changed

--- lib/features/service/view/pages/services_page.dart
+++ lib/features/service/view/pages/services_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hand_car/core/extension/theme_extension.dart';
 import 'package:hand_car/features/Home/view/widgets/drawer_widget.dart';
-import 'package:hand_car/features/service/view/widgets/service_button_widget.dart';
 import 'package:hand_car/features/service/view/widgets/service_info_container_widget.dart';
+import 'package:hand_car/features/service/view/widgets/services_icon_widget.dart';
 import 'package:hand_car/gen/assets.gen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -238,81 +238,3 @@ class ServicesPage extends HookConsumerWidget {
   }
 }
 
-class ServicesIconsWidget extends StatelessWidget {
-  final String image;
-  final String title;
-  final int selectedIndex;
-  final Function(int) onSelectService;
-  final bool isSelected;
-  const ServicesIconsWidget({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.selectedIndex,
-    required this.onSelectService,
-    required this.isSelected,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onSelectService(selectedIndex),
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: context.space.space_50),
-            decoration: BoxDecoration(
-                color: isSelected
-                    ? context.colors.primary
-                    : context.colors.background,
-                borderRadius: BorderRadius.circular(20)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: context.space.space_200,
-                      vertical: context.space.space_150),
-                  child: CircleAvatar(
-                    backgroundColor: isSelected
-                        ? context.colors.background
-                        : context.colors.primary,
-                    radius: 30,
-                    child: SvgPicture.asset(
-                      image,
-                      colorFilter: ColorFilter.mode(
-                        isSelected
-                            ? context.colors.primary
-                            : context.colors.background,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: context.space.space_200),
-                  child: Text(
-                    title,
-                    style: isSelected
-                        ? context.typography.bodyMedium.copyWith(
-                            color: isSelected
-                                ? context.colors.white
-                                : context.colors.primaryTxt,
-                          )
-                        : context.typography.body.copyWith(
-                            color: isSelected
-                                ? context.colors.white
-                                : context.colors.primaryTxt,
-                          ),
-                  ),
-                ),
-                SizedBox(height: context.space.space_150),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/features/service/view/widgets/services_icon_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hand_car/core/extension/theme_extension.dart';
+
+class ServicesIconsWidget extends StatelessWidget {
+  final String image;
+  final String title;
+  final int selectedIndex;
+  final Function(int) onSelectService;
+  final bool isSelected;
+  const ServicesIconsWidget({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.selectedIndex,
+    required this.onSelectService,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onSelectService(selectedIndex),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: context.space.space_50),
+            decoration: BoxDecoration(
+                color: isSelected
+                    ? context.colors.primary
+                    : context.colors.background,
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: context.space.space_200,
+                      vertical: context.space.space_150),
+                  child: CircleAvatar(
+                    backgroundColor: isSelected
+                        ? context.colors.background
+                        : context.colors.primary,
+                    radius: 30,
+                    child: SvgPicture.asset(
+                      image,
+                      colorFilter: ColorFilter.mode(
+                        isSelected
+                            ? context.colors.primary
+                            : context.colors.background,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: context.space.space_200),
+                  child: Text(
+                    title,
+                    style: isSelected
+                        ? context.typography.bodyMedium.copyWith(
+                            color: isSelected
+                                ? context.colors.white
+                                : context.colors.primaryTxt,
+                          )
+                        : context.typography.body.copyWith(
+                            color: isSelected
+                                ? context.colors.white
+                                : context.colors.primaryTxt,
+                          ),
+                  ),
+                ),
+                SizedBox(height: context.space.space_150),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
