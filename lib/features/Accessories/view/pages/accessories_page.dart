fc0a8755c8488ab39cb7fcdef7462bebd74efcea
risanpallithazha <risanpallithazha@gmail.com>
2024-12-04 11:07:14 +0530

    Products Filtering Changed

--- lib/features/Accessories/view/pages/accessories_page.dart
+++ lib/features/Accessories/view/pages/accessories_page.dart
@@ -14,6 +14,9 @@ import 'package:lottie/lottie.dart';
 
 final GlobalKey<ScaffoldState> scaffoldKey2 = GlobalKey<ScaffoldState>();
 
+// Provider to track selected category name
+final selectedCategoryNameProvider = StateProvider<String?>((ref) => null);
+
 class AccessoriesPage extends HookConsumerWidget {
   static const route = '/accessories';
   const AccessoriesPage({super.key});
@@ -26,26 +29,20 @@ class AccessoriesPage extends HookConsumerWidget {
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST_jdzAn0TttNbib1DGe119FjY-Wi_L5zc8g&s',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvOX4aF0GarSWiEFx7EP3sixmcfagZRL6zPg&s'
     ];
+    
     final controller = useScrollController();
     final appBarVisible = useState(true);
     final pageController = usePageController();
     final currentPage = useState(0);
     final isSearching = useState(false);
-
     final searchTextController = useTextEditingController();
     final category = ref.watch(categoryControllerProvider);
 
     useEffect(() {
-      /// Changes [appBarVisible] based on the scroll direction.
-      ///
-      /// If the user is scrolling down, hides the app bar.
-      /// If the user is scrolling up, shows the app bar.
       void onScroll() {
-        if (controller.position.userScrollDirection ==
-            ScrollDirection.reverse) {
+        if (controller.position.userScrollDirection == ScrollDirection.reverse) {
           appBarVisible.value = false;
-        } else if (controller.position.userScrollDirection ==
-            ScrollDirection.forward) {
+        } else if (controller.position.userScrollDirection == ScrollDirection.forward) {
           appBarVisible.value = true;
         }
       }
@@ -70,7 +67,7 @@ class AccessoriesPage extends HookConsumerWidget {
                         border: InputBorder.none,
                       ),
                       onChanged: (value) {
-                        // FiltNaver logic here
+                        // Search logic here
                       },
                     )
                   : const Text('Accessories'),
@@ -82,14 +79,10 @@ class AccessoriesPage extends HookConsumerWidget {
                     onPressed: () => isSearching.value = true,
                   ),
                 IconButton(
-                    onPressed: () {
-                      context.push('/cart');
-                    },
+                    onPressed: () => context.push('/cart'),
                     icon: const Icon(Icons.shopping_cart_sharp)),
                 IconButton(
-                    onPressed: () {
-                      scaffoldKey2.currentState?.openDrawer();
-                    },
+                    onPressed: () => scaffoldKey2.currentState?.openDrawer(),
                     icon: const Icon(Icons.menu)),
               ],
               leading: isSearching.value
@@ -111,9 +104,7 @@ class AccessoriesPage extends HookConsumerWidget {
             height: context.space.space_400 * 5,
             child: category.when(
               data: (categories) => ListView.separated(
-                separatorBuilder: (context, index) => const SizedBox(
-                  width: 10,
-                ),
+                separatorBuilder: (context, index) => const SizedBox(width: 10),
                 scrollDirection: Axis.horizontal,
                 itemCount: categories.length,
                 itemBuilder: (context, index) {
@@ -121,51 +112,49 @@ class AccessoriesPage extends HookConsumerWidget {
                   return AccessoriesCircleAvatharWidget(
                     text1: category.name,
                     image: images[index],
-                    onTap: () {},
+                    onTap: () {
+                      pageController.animateToPage(
+                        index,
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      );
+                      ref.read(selectedCategoryNameProvider.notifier).state = category.name;
+                    },
                   );
                 },
               ),
-              error: (error, _) =>
-                  Center(child: Lottie.asset(Assets.animations.error)),
+              error: (error, _) => Center(child: Lottie.asset(Assets.animations.error)),
               loading: () => const Center(child: CircularProgressIndicator()),
             ),
           ),
           Expanded(
-            child: PageView(
+            child: PageView.builder(
               controller: pageController,
               onPageChanged: (index) {
                 currentPage.value = index;
+                category.whenOrNull(
+                  data: (categories) {
+                    ref.read(selectedCategoryNameProvider.notifier).state = 
+                        categories[index].name;
+                  },
+                );
               },
-              children: [
-                // Maintenance & Care
-                // GridViewBuilderAccessoriesWidget(
-                //   name: "3M Car Washer",
-                //   price: '190',
-                //   discount: '298',
-                //   off: '30',
-                //   image:
-                //       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJnD1gtXHviN3Ywia6uOcoAfa1XHgCsh7Zkw&s',
-                // ),
-                // // Interior Accessories
-                // GridViewBuilderAccessoriesWidget(
-                //   name: "Car Seat",
-                //   price: '180',
-                //   discount: '299',
-                //   off: '30',
-                //   image:
-                //       'https://e7.pngegg.com/pngimages/372/436/png-clipart-white-and-black-vehicle-interior-car-seat-massage-chair-child-safety-seat-automotive-interior-driving-interior-design-thumbnail.png',
-                // ),
-                // // Electronics Accessories
-                // GridViewBuilderAccessoriesWidget(
-                //   name: "Unitopsci Wireless Bluetooth Speaker",
-                //   price: '120',
-                //   discount: '299',
-                //   off: '30',
-                //   image:
-                //       'https://www.pngplay.com/wp-content/uploads/7/Automobile-Car-Accessories-PNG-Background.png',
-                // ),
-                GridViewBuilderAccessoriesWidget()
-              ],
+              itemCount: category.whenOrNull(
+                data: (categories) => categories.length,
+              ) ?? 0,
+              itemBuilder: (context, index) {
+                return category.when(
+                  data: (categories) {
+                    return GridViewBuilderAccessoriesWidget(
+                      categoryName: categories[index].name,
+                    );
+                  },
+                  loading: () => const Center(child: CircularProgressIndicator()),
+                  error: (error, _) => Center(
+                    child: Lottie.asset(Assets.animations.error),
+                  ),
+                );
+              },
             ),
           ),
         ],
@@ -173,180 +162,3 @@ class AccessoriesPage extends HookConsumerWidget {
     );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-// import 'package:flutter_hooks/flutter_hooks.dart';
-// import 'package:flutter_svg/svg.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:hand_car/core/extension/theme_extension.dart';
-// import 'package:hand_car/features/Accessories/view/pages/product_search_page.dart';
-// import 'package:hand_car/features/Accessories/view/widgets/accessories/accessories_circle_avatar_widget.dart';
-// import 'package:hand_car/features/Accessories/view/widgets/accessories/grid_view_for_accessories_widget.dart';
-// import 'package:hand_car/features/Accessories/controller/products_controller/products_controller.dart';
-// import 'package:hand_car/gen/assets.gen.dart';
-
-// final GlobalKey<ScaffoldState> scaffoldKey2 = GlobalKey<ScaffoldState>();
-
-// class AccessoriesPage extends HookConsumerWidget {
-//   static const route = '/accessories';
-//   const AccessoriesPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final controller = useScrollController();
-//     final appBarVisible = useState(true);
-//     final pageController = usePageController(initialPage: 0);
-//     final currentPage = useState(0);
-//     final isSearching = useState(false);
-//     final searchTextController = useTextEditingController();
-
-//     // Listen to products state
-//     final productsState = ref.watch(productsControllerProvider);
-
-//     useEffect(() {
-//       void onScroll() {
-//         if (controller.position.userScrollDirection == ScrollDirection.reverse) {
-//           appBarVisible.value = false;
-//         } else if (controller.position.userScrollDirection == ScrollDirection.forward) {
-//           appBarVisible.value = true;
-//         }
-//       }
-
-//       controller.addListener(onScroll);
-//       return () => controller.removeListener(onScroll);
-//     }, [controller]);
-
-//     // Search handler
-//     void handleSearch(String query) {
-//       ref.read(productsControllerProvider.notifier).searchProducts(query);
-//     }
-
-//     return Scaffold(
-//       key: scaffoldKey2,
-//       appBar: appBarVisible.value
-//           ? AppBar(
-//               backgroundColor: context.colors.white,
-//               title: isSearching.value
-//                   ? TextField(
-//                       controller: searchTextController,
-//                       autofocus: true,
-//                       decoration: InputDecoration(
-//                         suffixIcon: const Icon(Icons.search),
-//                         hintText: 'Search Accessories',
-//                         hintStyle: context.typography.body,
-//                         border: InputBorder.none,
-//                       ),
-//                       onChanged: handleSearch,
-//                     )
-//                   : const Text('Accessories'),
-//               centerTitle: true,
-//               actions: [
-//                 if (!isSearching.value)
-//                   IconButton(
-//                     icon: const Icon(Icons.search),
-//                     onPressed: () => isSearching.value = true,
-//                   ),
-//                 IconButton(
-//                     onPressed: () {
-//                       context.push('/cart');
-//                     },
-//                     icon: const Icon(Icons.shopping_cart_sharp)),
-//                 IconButton(
-//                     onPressed: () {
-//                       scaffoldKey2.currentState?.openDrawer();
-//                     },
-//                     icon: const Icon(Icons.menu)),
-//               ],
-//               leading: isSearching.value
-//                   ? IconButton(
-//                       icon: const Icon(Icons.arrow_back),
-//                       onPressed: () {
-//                         isSearching.value = false;
-//                         searchTextController.clear();
-//                         ref.read(productsControllerProvider.notifier).searchProducts('');
-//                       },
-//                     )
-//                   : SvgPicture.asset('assets/icons/hand_car_icon.svg'),
-//             )
-//           : null,
-//       body: Column(
-//         children: [
-//           SizedBox(
-//             height: context.space.space_400 * 5,
-//             child: ListView(
-//               scrollDirection: Axis.horizontal,
-//               children: [
-//                 AccessoriesCircleAvatharWidget(
-//                   onTap: () => pageController.animateToPage(0,
-//                       duration: const Duration(milliseconds: 300),
-//                       curve: Curves.easeInOut),
-//                   image: Assets.images.imgCarCareAccessories.path,
-//                   text1: 'Maintenance &',
-//                   text2: 'Care',
-//                 ),
-//                 SizedBox(width: context.space.space_250),
-//                 AccessoriesCircleAvatharWidget(
-//                   onTap: () => pageController.animateToPage(1,
-//                       duration: const Duration(milliseconds: 300),
-//                       curve: Curves.easeInOut),
-//                   image: Assets.images.imgCarInteriorAccessories.path,
-//                   text1: 'Interior',
-//                   text2: 'Accessories',
-//                 ),
-//                 SizedBox(width: context.space.space_250),
-//                 AccessoriesCircleAvatharWidget(
-//                   onTap: () => pageController.animateToPage(2,
-//                       duration: const Duration(milliseconds: 300),
-//                       curve: Curves.easeInOut),
-//                   image: Assets.images.imgCarSpeaker.path,
-//                   text1: 'Electronics',
-//                   text2: 'Accessories',
-//                 ),
-//                 SizedBox(width: context.space.space_250),
-//                 AccessoriesCircleAvatharWidget(
-//                   onTap: () => pageController.animateToPage(3,
-//                       duration: const Duration(milliseconds: 300),
-//                       curve: Curves.easeInOut),
-//                   image: Assets.images.imgCarSpeaker.path,
-//                   text1: 'Other',
-//                   text2: 'Accessories',
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Expanded(
-//             child: productsState.when(
-//               data: (products) => GridView.builder(
-//   key: PageStorageKey<String>('productsGrid'),
-//   controller: controller, // Ensure this is only created once
-//   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//     crossAxisCount: 2,
-//     crossAxisSpacing: 10,
-//     mainAxisSpacing: 10,
-//     childAspectRatio: 0.7,
-//   ),
-//   itemCount: products.length,
-//   itemBuilder: (context, index) {
-//     final product = products[index];
-//     return GridViewBuilderAccessoriesWidget(
-//       key: ValueKey(product.id), // Use unique values for keys
-//       name: product.name,
-//       price: product.price,
-//       image: product.image ?? '',
-//       discount:   '', // Assuming these fields exist
-//       off:  '',
-//     );
-//   },
-// ),
-//               loading: () => const Center(child: CircularProgressIndicator()),
-//               error: (error, stack) => Center(
-//                 child: Text('Error: $error'),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
--- lib/features/Accessories/view/widgets/accessories/grid_view_for_accessories_widget.dart
+++ lib/features/Accessories/view/widgets/accessories/grid_view_for_accessories_widget.dart
@@ -1,45 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:hand_car/features/Accessories/controller/products_controller/products_controller.dart';
 import 'package:hand_car/features/Accessories/view/widgets/accessories/accessories_product_card_widget.dart';
 import 'package:hand_car/gen/assets.gen.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lottie/lottie.dart';
 
 class GridViewBuilderAccessoriesWidget extends ConsumerWidget {
-  const GridViewBuilderAccessoriesWidget({super.key});
+  final String categoryName;
+
+  const GridViewBuilderAccessoriesWidget({
+    required this.categoryName,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final productsState = ref.watch(productsControllerProvider);
 
-    // Using the AsyncValue to handle the loading, error, and data states
     return productsState.when(
       loading: () => const Center(child: CircularProgressIndicator()),
       error: (error, _) => Center(child: Lottie.asset(Assets.animations.error)),
-      data: (products) => GridView.builder(
-        shrinkWrap: true,
-        physics:
-            const ClampingScrollPhysics(), // Better for expanding inside Column
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 340,
-          mainAxisSpacing: 0.5,
-          mainAxisExtent: 380,
-          crossAxisSpacing: 1.7,
-        ),
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return AccessoriesProductCardWidget(
-            name: product.name,
-            price: "AED ${product.price}",
-            image:
-                'https://w7.pngwing.com/pngs/865/117/png-transparent-loudspeaker-enclosure-stereophonic-sound-amplifier-subwoofer-music-speakers-three-black-speakers-electronics-speaker-music-note-thumbnail.png',
-            discount: '',
-            off: '',
-            productId: product.id,
-          );
-        },
-      ),
+      data: (products) {
+        // Filter products by category name
+        final categoryProducts = products
+            .where((product) => product.category.toLowerCase() == categoryName.toLowerCase())
+            .toList();
+
+        return GridView.builder(
+          shrinkWrap: true,
+          physics: const ClampingScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 340,
+            mainAxisSpacing: 0.5,
+            mainAxisExtent: 380,
+            crossAxisSpacing: 1.7,
+          ),
+          itemCount: categoryProducts.length,
+          itemBuilder: (context, index) {
+            final product = categoryProducts[index];
+            return AccessoriesProductCardWidget(
+              name: product.name,
+              price: "AED ${product.price}",
+              image: product.image ?? 'https://w7.pngwing.com/pngs/865/117/png-transparent-loudspeaker-enclosure-stereophonic-sound-amplifier-subwoofer-music-speakers-three-black-speakers-electronics-speaker-music-note-thumbnail.png',
+              discount: '',
+              off: '',
+              productId: product.id,
+            );
+          },
+        );
+      },
     );
   }
 }
