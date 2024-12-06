0d4a4cbf60615055c6587158b6e148fc162ace34
risanpallithazha <risanpallithazha@gmail.com>
2024-12-06 10:33:54 +0530

    plan Services Added

--- lib/features/Subscriptions/service/plan_service.dart
+++ lib/features/Subscriptions/service/plan_service.dart
@@ -1,7 +1,23 @@
 import 'package:dio/dio.dart';
+import 'package:hand_car/config.dart';
+import 'package:hand_car/features/Subscriptions/controller/model/plan_model.dart';
 
 class PlanServices {
-  static Dio _dio = Dio(
-  
+  static final Dio _dio = Dio(
+  BaseOptions(
+    baseUrl: baseUrl,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  )
   );
+
+  static Future<PlanModel> getSubscription() async {
+    try {
+      final response = await _dio.get('/subscribe/');
+      return PlanModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception('Failed to fetch products: ${e.message}');
+    }
+  }
 }
