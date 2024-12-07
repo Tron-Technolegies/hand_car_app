807efe755104284734a1ec8b20d3c7c0273e889b
risanpallithazha <risanpallithazha@gmail.com>
2024-12-07 14:26:47 +0530

    Coupon input updated

--- lib/features/Accessories/view/pages/cart_page.dart
+++ lib/features/Accessories/view/pages/cart_page.dart
@@ -12,140 +12,9 @@ import 'package:hand_car/gen/assets.gen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lottie/lottie.dart';
 
-// //Shopping Cart Screen
-// class ShoppingCartScreen extends HookConsumerWidget {
-//   static const route = '/cart_page';
-//   const ShoppingCartScreen({super.key});
+// c
+import 'package:hand_car/features/Accessories/controller/coupon/coupon_controller.dart';
 
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     // Access the cart controller
-//     final cartController = ref.watch(cartControllerProvider);
-//     // Access the coupon text controller
-//     final couponController = useTextEditingController();
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Your Shopping Cart', style: context.typography.h3),
-//         actions: [
-//           if (cartController.asData?.value.cartItems.isNotEmpty ?? false)
-//             IconButton(
-//               icon: const Icon(Icons.delete_outline),
-//               onPressed: () {
-//                 // Add your clear cart logic here
-//               },
-//             ),
-//         ],
-//       ),
-//       body: SafeArea(
-//         child: SingleChildScrollView(
-//           child: Padding(
-//             padding: EdgeInsets.all(context.space.space_200),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 cartController.when(
-//                   data: (cart) {
-//                     if (cart.cartItems.isEmpty) {
-//                       return Center(
-//                         child: Column(
-//                           mainAxisAlignment: MainAxisAlignment.center,
-//                           children: [
-//                             Lottie.asset(Assets.animations.emptyCart,
-//                                 repeat: false),
-//                             SizedBox(height: context.space.space_100),
-//                             Text(
-//                               "Your cart is empty",
-//                               style: context.typography.h3.copyWith(
-//                                 color: context.colors.primaryTxt,
-//                               ),
-//                               textAlign: TextAlign.center,
-//                             ),
-//                           ],
-//                         ),
-//                       );
-//                     }
-
-//                     return ListView.separated(
-//                       shrinkWrap: true,
-//                       physics: const NeverScrollableScrollPhysics(),
-//                       itemCount: cart.cartItems.length,
-//                       separatorBuilder: (_, __) => SizedBox(
-//                         height: context.space.space_100,
-//                       ),
-//                       itemBuilder: (context, index) {
-//                         final item = cart.cartItems[index];
-//                         return ProductCard(
-//                           productName: item.productName,
-//                           price: item.productPrice,
-//                           quantity: item.quantity,
-//                           image:
-//                               'https://e7.pngegg.com/pngimages/809/777/png-clipart-car-revathy-auto-parts-ford-motor-company-spare-part-advance-auto-parts-car-car-vehicle-thumbnail.png',
-//                         );
-//                       },
-//                     );
-//                   },
-//                   loading: () => Center(
-//                     child: Lottie.asset(Assets.animations.loading),
-//                   ),
-//                   error: (error, _) => Center(
-//                     child: Column(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: [
-//                         Lottie.asset(Assets.animations.error, height: 150),
-//                         SizedBox(height: context.space.space_100),
-//                         Text(
-//                           "Failed to load cart: $error",
-//                           style: context.typography.h3.copyWith(
-//                             color: context.colors.primaryTxt,
-//                           ),
-//                           textAlign: TextAlign.center,
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//                 if (cartController.asData?.value.cartItems.isNotEmpty ?? false)
-//                   Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       SizedBox(height: context.space.space_200),
-//                       CouponCardListView(),
-//                       CouponInputSection(controller: couponController),
-//                       TotalAmountSectionWidget(
-//                         grandTotal: cartController.asData!.value.totalAmount,
-//                         delivery: 0,
-//                         total: cartController.asData!.value.totalAmount,
-//                       ),
-//                     ],
-//                   ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//       bottomNavigationBar:
-//           cartController.asData?.value.cartItems.isNotEmpty ?? false
-//               ? Padding(
-//                   padding: EdgeInsets.all(context.space.space_200),
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: [
-//                       Text(
-//                         'Total: AED ${cartController.asData!.value.totalAmount.toStringAsFixed(2)}',
-//                         style: context.typography.bodyMedium,
-//                       ),
-//                       ButtonWidget(
-//                         label: 'Proceed To Checkout',
-//                         onTap: () => context.go('/checkout'),
-//                       ),
-//                     ],
-//                   ),
-//                 )
-//               : null,
-//     );
-//   }
-// }
 class ShoppingCartScreen extends HookConsumerWidget {
   static const route = '/cart_page';
   const ShoppingCartScreen({super.key});
@@ -163,7 +32,7 @@ class ShoppingCartScreen extends HookConsumerWidget {
             IconButton(
               icon: const Icon(Icons.delete_outline),
               onPressed: () {
-                // Add your clear cart logic here
+           
               },
             ),
         ],
@@ -243,12 +112,34 @@ class ShoppingCartScreen extends HookConsumerWidget {
                       SizedBox(height: context.space.space_200),
                       CouponCardListView(
                         onCouponApply: (coupon) {
+                          couponController.text = coupon.couponCode;
                           ref
                               .read(cartControllerProvider.notifier)
                               .applyCoupon(coupon);
                         },
                       ),
-                      CouponInputSection(controller: couponController),
+                      CouponInputSection(
+                        controller: couponController,
+                        onApply: (code) {
+                          final validCoupon = ref
+                              .read(couponControllerProvider)
+                              .asData
+                              ?.value
+                              .where((coupon) => coupon.couponCode == code)
+                              .firstOrNull;
+                          if (validCoupon != null) {
+                            ref
+                                .read(cartControllerProvider.notifier)
+                                .applyCoupon(validCoupon);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Invalid Coupon Code!'),
+                              ),
+                            );
+                          }
+                        },
+                      ),
                       Consumer(
                         builder: (_, ref, __) {
                           final cart = ref.watch(cartControllerProvider).value;
--- lib/features/Accessories/view/widgets/coupon/coupon_input_widget.dart
+++ lib/features/Accessories/view/widgets/coupon/coupon_input_widget.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+// For Clipboard
 import 'package:hand_car/core/extension/theme_extension.dart';
 import 'package:hand_car/core/widgets/button_widget.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class CouponInputSection extends HookConsumerWidget {
   final TextEditingController controller;
+  final void Function(String code) onApply;
 
-  const CouponInputSection({super.key, required this.controller});
+  const CouponInputSection({
+    super.key,
+    required this.controller,
+    required this.onApply,
+  });
 
   @override
-  Widget build(BuildContext context,ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
+
     return Padding(
       padding: EdgeInsets.all(context.space.space_200),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'Do you have any Coupon code ?',
+            'Do you have any Coupon code?',
             style: context.typography.bodyLarge,
           ),
           SizedBox(height: context.space.space_100),
@@ -32,7 +40,10 @@ class CouponInputSection extends HookConsumerWidget {
                 ),
               ),
               SizedBox(width: context.space.space_200),
-              ButtonWidget(label: "Apply", onTap: () {})
+              ButtonWidget(
+                label: "Apply",
+                onTap: () => onApply(controller.text.trim()),
+              ),
             ],
           ),
         ],
