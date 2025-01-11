41328a532b0e3f493a1917f8b2ef049b311af6aa
risanpallithazha <risanpallithazha@gmail.com>
2025-01-11 15:22:35 +0530

    Car service api created

---
+++ lib/features/car_service/service/car_service_api_service.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:hand_car/config.dart';
+import 'package:hand_car/features/car_service/model/service_model.dart';
+
+class CarServiceApiService {
+  static final Dio _dio=Dio(
+    BaseOptions(
+      baseUrl: baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ),
+
+  );
+
+  Future<List<ServiceModel>>getService()async{
+    try {
+       final response=await _dio.get('/view_services');
+       if(response.statusCode==200){
+        final List<dynamic>serviceList=response.data['Service List:'];
+        return serviceList.map((service) => ServiceModel.fromJson(service)).toList();
+       }
+       throw Exception('Failed to fetch services');
+    } catch (e) {
+      throw Exception('Failed to fetch services: $e');
+    }
+  }
+}
